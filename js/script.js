75af7ef09d503308dbc0686a673134bf6f54b500
Joanna Szlaska <joanna.szlaska8@gmail.com>
2022-06-27 18:03:13 +0200

    js class graph and initial js added

---
+++ js/script.js
@@ -0,0 +1,33 @@
+{
+    const tasks = [
+        {
+            content: "zrobić trening",
+            done: false,
+        },
+
+        {
+            content: "zjeść kolację",
+            done: true,
+        },
+    ]
+
+    const render = () => {
+     let htmlString = "";
+
+     for(const task of tasks) {
+        htmlString += `
+        <li>
+        ${task.content}
+        </li> `
+        ;
+
+     }
+     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    };
+
+    const init = () => {
+        render();
+    }
+    init();
+}
