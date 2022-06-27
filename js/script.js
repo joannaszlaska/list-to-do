f42d760511b170557a3c289dba6c667ad1d67302
Joanna Szlaska <joanna.szlaska8@gmail.com>
2022-06-27 20:08:23 +0200

    adding new tasks showing if a task is done adding remove tasks button

--- js/script.js
+++ js/script.js
@@ -16,18 +16,52 @@
 
      for(const task of tasks) {
         htmlString += `
-        <li>
+        <button class="js-done">zrobione</button>
+        <li class="list__item list__item--done" ${task.done ? "list__item--done" : "" }>
         ${task.content}
+        <button class="js-remove">usuń</button>
         </li> `
         ;
 
      }
      document.querySelector(".js-tasks").innerHTML = htmlString;
 
+     const removeButtons = document.querySelectorAll(".js-remove");
+    
+     removeButtons.forEach((removeButtons, index) => {
+        removeButtons.addEventListener("click", () => {
+            tasks.splice(index, 1);
+            render();
+        })
+     })
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+    render();
+    };
+
+    const onFormSubmit =
+        (event) => {
+            event.preventDefault(); 
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+            addNewTask(newTaskContent);
+
+        };
+    
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        
+        form.addEventListener("submit", onFormSubmit)
     }
     init();
 }
